1c02ab74a8e77d4803abf284b593a1f5c046f32c
George Artemiev <applecity@Apples-MBP.Dlink>
2023-08-11 18:57:47 +0300

    Switch deployemnt to argocd-apps

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,12 +9,13 @@ resource "helm_release" "argo_cd" {
   ]
 }
 
-resource "helm_release" "infra" {
-  name              = "infra"
-  chart             = "../infra"
-  namespace         = "argo-cd"
-  create_namespace  = true
-  dependency_update = true
+resource "helm_release" "argo_apps" {
+  name             = "argo-apps"
+  chart            = "argocd-apps"
+  repository       = "https://argoproj.github.io/argo-helm"
+  namespace        = "argo-cd"
+  create_namespace = true
+  version          = "1.4.1"
   values = [
     file("../infra/values.yaml")
   ]
